43578d9d08336d7ef6a4a9ea28e9473638f797d2
Perry Kibler <pk@unleashed.io>
2015-01-07 16:35:28 -0700

    another route, but this needs to be dynamic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Savory' });
+});
+
+router.get('/get-involved', function(req, res) {
+  res.render('get-involved/index', { title: 'Express' });
 });
 
 module.exports = router;
