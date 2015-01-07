5366631abd14cd9b08a22ba90b35e01193da6591
Perry Kibler <pk@unleashed.io>
2015-01-07 09:40:27 -0700

    added assets from other folder

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,1 +1,100 @@
-$(document).foundation();
+//= require modernizr
+//= require jquery
+//= require foundation.min
+//= require fastclick
+//= require owl.carousel.min
+
+$(function() {
+
+  $(document).foundation();
+
+  // smooth scrolling
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+  });
+
+
+  // nav helper
+  // when you click an anchor from the nav, the off-canvas bar should close
+  $(".close-off-canvas").on("click.toggleCanvas", function(){
+    $(".exit-off-canvas").click();
+  });
+
+
+  // calling in mapbox
+  if( document.getElementById('map') != null ) {
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic2F2b3J5aW5zdGl0dXRlIiwiYSI6ImFJbDI3Qm8ifQ.usgK1Wk7eQAbG6Jjp7knGQ';
+    var map = L.mapbox.map('map', 'savoryinstitute.jjdf51a0');
+
+    map.dragging.disable();
+    map.touchZoom.disable();
+    // map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+
+    // Disable tap handler, if present.
+    if (map.tap) map.tap.disable();
+  }
+
+  // http://www.owlcarousel.owlgraphic.com/docs
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: true
+  });
+
+
+
+});
+
+
+
+// user voice stuff
+// Include the UserVoice JavaScript SDK (only needed once on a page)
+UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/Rs5HAgInY34TobrNnCxSDQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+
+//
+// UserVoice Javascript SDK developer documentation:
+// https://www.uservoice.com/o/javascript-sdk
+//
+
+// Set colors
+UserVoice.push(['set', {
+  accent_color: '#808283',
+  trigger_color: 'white',
+  trigger_background_color: 'rgba(46, 49, 51, 0.6)'
+}]);
+
+// Identify the user and pass traits
+// To enable, replace sample data with actual user traits and uncomment the line
+UserVoice.push(['identify', {
+  //email:      'john.doe@example.com', // User’s email address
+  //name:       'John Doe', // User’s real name
+  //created_at: 1364406966, // Unix timestamp for the date the user signed up
+  //id:         123, // Optional: Unique id of the user (if set, this should not change)
+  //type:       'Owner', // Optional: segment your users by type
+  //account: {
+  //  id:           123, // Optional: associate multiple users with a single account
+  //  name:         'Acme, Co.', // Account name
+  //  created_at:   1364406966, // Unix timestamp for the date the account was created
+  //  monthly_rate: 9.99, // Decimal; monthly rate of the account
+  //  ltv:          1495.00, // Decimal; lifetime value of the account
+  //  plan:         'Enhanced' // Plan name for the account
+  //}
+}]);
+
+// Add default trigger to the bottom-right corner of the window:
+UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'bottom-right' }]);
+
+// Or, use your own custom trigger:
+//UserVoice.push(['addTrigger', '#id', { mode: 'contact' }]);
+
+// Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
+UserVoice.push(['autoprompt', {}]);
