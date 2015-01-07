872d1473679c52f30c7961512371d2f457e405d0
Bryan Shelton <bryan@rover.com>
2015-01-07 08:49:44 -0800

    Gulp dev server

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,22 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var server = require('gulp-express');
+var nodemon = require('gulp-nodemon');
 
 
-sassPaths = [
+sass_include_paths = [
   'assets/bower_components/foundation/scss',
 ]
+
+function notify_live_reload(e) {
+  gulp.src(e.path, {read: false})
+  .pipe(livereload());
+}
 
 gulp.task('sass', function() {
   return gulp.src('assets/stylesheets/app.scss')
     .pipe(sass({
-      includePaths: sassPaths,
+      includePaths: sass_include_paths,
       outputStyle: 'compressed',
       errLogToConsole: true
     }))
@@ -25,11 +31,6 @@ gulp.task('sass', function() {
     .pipe(livereload());
 });
 
-function notify_live_reload(e) {
-  gulp.src(e.path, {read: false})
-    .pipe(livereload());
-}
-
 gulp.task('watch', function() {
   gulp.watch(['views/**/*.jade'], notify_live_reload);
   gulp.watch(['assets/stylesheets/**/*.scss'], ['sass']);
@@ -37,4 +38,12 @@ gulp.task('watch', function() {
 
 gulp.task('default', ['sass', 'watch'], function() {
   livereload.listen();
+  nodemon({
+    ext: 'js',
+    ignore: ['public/**',  'views/**', 'assets/**', 'bin/**'],
+    script: 'bin/dev-server'
+  })
+  .on('restart', function() {
+    console.log('Restarted server');
+  });
 });
