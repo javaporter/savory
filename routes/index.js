b29e8c4eeef515bd8cad6bbb14456370b1537ad4
Perry Kibler <pk@unleashed.io>
2015-01-07 18:20:46 -0700

    added more routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Savory' });
 });
 
 router.get('/get-involved', function(req, res) {
   res.render('get-involved/index', { title: 'Express' });
+});
+
+router.get('/institute', function(req, res) {
+  res.render('institute/index', { title: 'Institute' });
+});
+
+router.get('/platform', function(req, res) {
+  res.render('platform/index', { title: 'Platform' });
+});
+
+router.get('/network', function(req, res) {
+  res.render('network/index', { title: 'Network' });
 });
 
 module.exports = router;
