f52d1cdc4f48d1ceab85e08195f8c014c9e3da4e
Perry Kibler <pk@unleashed.io>
2015-01-08 12:57:00 -0700

    forcing js refresh

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -48,7 +48,6 @@ $(function() {
 });
 
 
-
 // user voice stuff
 // Include the UserVoice JavaScript SDK (only needed once on a page)
 UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/Rs5HAgInY34TobrNnCxSDQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
