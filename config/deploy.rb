7b76ceb6beaa4fc5667d4ce51a574174c1c4eeca
Bryan Shelton <bryan@sheltonplace.com>
2015-01-11 12:43:37 -0800

    Fix cap deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,10 @@ lock '3.3.5'
 
 set :application, 'savory'
 set :branch, :master
-set :repo_url, 'git@github.com:javaporter/savory.git'
+set :repo_url, 'savory-github:javaporter/savory.git'
 set :keep_releases, 2
+
+set :linked_dirs, %w{node_modules}
 
 namespace :deploy do
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server '107.170.243.33', user: 'savory', roles: %w{app}, deploy_to: '/home/savory/apps/savory-node'
+set :deploy_to, '/home/savory/apps/savory-node'
+server '107.170.243.33', user: 'savory', roles: %w{app}
