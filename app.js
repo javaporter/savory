cd8d64009535063e5099fdd71fc381fbe8aa61b1
Bryan Shelton <bryan@sheltonplace.com>
2015-01-17 10:50:58 -0800

    Only serve static from express in development.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+if (app.get('env') === 'development') {
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
 app.locals.basedir = path.join(__dirname, 'views');
 
