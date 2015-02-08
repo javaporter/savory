63dd2d6c1c37eef0d7be8a23e29d87a50cd8eeb8
Perry Kibler <pk@unleashed.io>
2015-02-07 19:02:26 -0700

    nav working again for desktop

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -76,12 +76,12 @@ $(function() {
 
 
     // off canvas accordion
-    $(".off-canvas-submenu").hide();
-    $(".off-canvas-submenu-call").click(function() {
-      var icon = $(this).parent().next(".off-canvas-submenu").is(':visible') ? '+' : '-';
-      $(this).parent().next(".off-canvas-submenu").slideToggle('fast');
-      $(this).find("span").text(icon);
-    });
+    // $(".off-canvas-submenu").hide();
+    // $(".off-canvas-submenu-call").click(function() {
+    //   var icon = $(this).parent().next(".off-canvas-submenu").is(':visible') ? '+' : '-';
+    //   $(this).parent().next(".off-canvas-submenu").slideToggle('fast');
+    //   $(this).find("span").text(icon);
+    // });
 
 });
 
