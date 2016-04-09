17d0804099240004b7b94cc2bfde7deda57d48a7
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-04-09 14:24:12 -0500

    savory journey photos and other small edits

--- routes/index.js
+++ routes/index.js
@@ -190,6 +190,7 @@ router.get('/savory-journeys/:journey', function(req,res){
   var journey = req.params.journey;
 
   res.render('savory-journey-template', {journeyTitle : journeys[journey]['title'],
+    headerPicture : journeys[journey]['headerPicture'],
     journeysubtitle : journeys[journey]['subtitle'], journeyDate : journeys[journey]['date'],
     journeyAvailability : journeys[journey]['availability'],
     journeyDescription : journeys[journey]['description'],
