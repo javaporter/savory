d757add7d012de1d59be180418772f1003db3bf9
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-06-09 11:00:17 -0600

    changing deploy user name

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,4 @@
 set :branch, ENV['BRANCH'] || 'dev'
 set :deploy_to, '/home/savory/apps/savory-dev'
 set :upstart_name, 'savory-dev'
-server '107.170.243.33', user: 'savory', roles: %w{app web}
+server '107.170.243.33', user: 'root', roles: %w{app web}
